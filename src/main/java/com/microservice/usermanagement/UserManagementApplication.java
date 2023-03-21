b6b576a515b9393c80c286370f76351763e55d3b
shubham21097 <shubham21097@gmail.com>
2023-03-21 22:20:02 +0530

    CORS configuration + security class

--- src/main/java/com/microservice/usermanagement/UserManagementApplication.java
+++ src/main/java/com/microservice/usermanagement/UserManagementApplication.java
@@ -13,14 +13,4 @@ public class UserManagementApplication {
 		SpringApplication.run(UserManagementApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8100");
-			}
-		};
-	}
-
 }
