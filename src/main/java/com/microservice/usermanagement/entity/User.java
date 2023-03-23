4cdcedf839e3a6c2b378052978dec6830aa88a8d
shubham21097 <shubham21097@gmail.com>
2023-03-24 00:18:12 +0530

    Adding Roles for User

--- src/main/java/com/microservice/usermanagement/entity/User.java
+++ src/main/java/com/microservice/usermanagement/entity/User.java
@@ -15,4 +15,5 @@ public class User {
     private String fullName;
     private String email;
     private String password;
+    private String role;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,17 @@
-INSERT INTO USERS(id, full_name, email, password)
-VALUES('sdflajadlkf', 'User1', 'user1@gmail.com', 'password');
+INSERT INTO USERS(id, full_name, email, password, roles)
+VALUES('sdflajadlkf', 'User1', 'user1@gmail.com', 'password', 'Administrator');
 
-INSERT INTO USERS(id, full_name, email, password)
-VALUES('aadfjkhnkv', 'User2', 'user2@gmail.com', 'password');
+INSERT INTO USERS(id, full_name, email, password, roles)
+VALUES('aadfjkhnkv', 'User2', 'user2@gmail.com', 'password', 'Manager');
 
-INSERT INTO USERS(id, full_name, email, password)
-VALUES('ernhkvjnkda', 'User3', 'user3@gmail.com', 'password');
+INSERT INTO USERS(id, full_name, email, password, roles)
+VALUES('ernhkvjnkda', 'User3', 'user3@gmail.com', 'password', 'Administrator');
 
-INSERT INTO USERS(id, full_name, email, password)
-VALUES('oioiajdifnk', 'User4', 'user4@gmail.com', 'password');
+INSERT INTO USERS(id, full_name, email, password, roles)
+VALUES('oioiajdifnk', 'User4', 'user4@gmail.com', 'password', 'Report Viewer');
 
-INSERT INTO USERS(id, full_name, email, password)
-VALUES('aerninvcdsi', 'User5', 'user5@gmail.com', 'password');
+INSERT INTO USERS(id, full_name, email, password, roles)
+VALUES('aerninvcdsi', 'User5', 'user5@gmail.com', 'password', 'Report Viewer');
+
+INSERT INTO USERS(id, full_name, email, password, roles)
+VALUES('fsdgesafdff', 'User6', 'user6@gmail.com', 'password', 'Manager');
